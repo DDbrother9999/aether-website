1ecd1c3f89180ef9887ca34dd7d48e66b1c45638
DDbrother <derekyin42009@gmail.com>
2025-11-29 12:28:13 -0500

    chore: init

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+    return (
+        <div className="min-h-screen font-sans text-white selection:bg-yellow-300 selection:text-yellow-900">
+            <div className="fixed inset-0 z-[-1] pointer-events-none bg-aether-sky">
+            </div>
+        </div>
+    );
+}
